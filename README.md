363a3aa4defaaac6b7a7c76f759b8415e5113d62
Takuya Nishimoto <nishimotz@gmail.com>
2024-11-15 09:59:19 +0900

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# hello-bootstrap-tailwind
+
+This is a simple project to learn how to use Bootstrap and Tailwind CSS together.
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
